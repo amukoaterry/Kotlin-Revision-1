53802f949c1d7f416a8655e2908b94468b4e58ea
amukoaterry <amukoaterrymitchell@gmail>
2024-03-13 16:24:14 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,97 @@
+import java.security.spec.MGF1ParameterSpec
+
+///KOTLIN OPERATORS//
+
+// /ARITHMETIC OPERATORS REVISION//
+
+fun main(){
+var num1 = 30
+var num2 = 50
+var sum = num1 + num2
+println(sum)
+
+    var num3 = 60
+    var num4 = 30
+    var difference = num3 - num4
+    println(difference)
+
+    var num5 = 5
+    var num6 = 10
+    var multiply = num5 * num6
+    println(multiply)
+
+    var x = 10
+    var y = 5
+    var result= x / y
+    println(result)
+
+    var s = 20
+    var p = 6
+    var modulus = s % p
+    println(modulus)
+
+    //ASSIGNMENT OPERATORS//
+
+    //COMPOUND ASSIGNMENT OPERATOR//
+
+  var  h = 2
+    var i = 1
+    println("h:" + h)
+    h += i
+    println("h:" + h)
+
+    var f = 6
+    var t = 8
+    println(f)
+
+    f += t
+    println(f)
+
+//INCREMENT AND DECREMENT ASSIGNMENT OPERATOR//
+
+//1.)Increment//
+
+
+    var age = 19
+    ++age
+    println(age)
+
+    //2.)Decrement//
+
+    var weight = 52
+    --weight
+    println(weight)
+
+    //COMPARISON OPERATOR//
+
+    //Greater Than & Less Than//
+
+    val a = 325
+    val b = 432
+    println(a>b)
+    println(a<b)
+
+//Greater than or equal to & less than or equal to//
+
+    val w = 325
+    val v = 432
+    println(w>=v)
+    println(w<=v)
+
+
+    //Is equal to & Is not equal to//
+
+    val k = 16
+    val j = 15
+    println(k==j)
+    println(k!=j)
+
+
+
+
+}
+
+
+
+
+
